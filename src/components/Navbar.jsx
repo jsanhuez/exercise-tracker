36ab5e46b5e4d385d5c0f8389be3858170870553
JuanSR <jfrancisco1717@gmail.com>
2020-06-02 23:15:08 -0400

    Change navbar name elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-            <Link to='/' className='navbar-brand'>ExerciseTracker</Link>
+            <Link to='/' className='navbar-brand'>Exercise Tracker</Link>
             <div className='collpase navbar-collapse'>
                 <ul className='navbar-nav mr-auto'>
                     <li className='navbar-item'>
                         <Link to='/' className='nav-link'>Exercises</Link>
                     </li>
                     <li className='navbar-item'>
-                        <Link to='/create' className='nav-link'>Create Exercise Log</Link>
+                        <Link to='/create' className='nav-link'>Create Exercise</Link>
                     </li>
                     <li className='navbar-item'>
                         <Link to='/user' className='nav-link'>Create User</Link>
